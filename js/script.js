27e90e8e5b1d4cde5e6af6bc99c7ff8d65fa6a30
jared Long <jlong4223@gmail.com>
2020-10-22 14:55:46 -0500

    connected to API

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
 // https://api.openweathermap.org/data/2.5/weather?q=London&appid=f68afd7e0d3f9ad0b336b4f6416210e9
 
-
+console.log("It's working!")
+function handleGetData(event){
+  event.preventDefault();
+  const searchText = $("#search").val()
 $.ajax({
-     url: `https://api.openweathermap.org/data/2.5/weather?q=Austin&appid=f68afd7e0d3f9ad0b336b4f6416210e9`
+     url: `https://api.openweathermap.org/data/2.5/weather?q=${searchText}&appid=f68afd7e0d3f9ad0b336b4f6416210e9`
      }) .then(
             (data) => {
         console.log(data)
-            })
+        $("#city").text(data.Main)
+        $("#temperature").text(data.Temp)
+        $("#feels").text(data.Feels_like)
+        $("#weather").text(data.Weather)
+      },
+      (error) => {
+        console.log("bad request: ", error)
+      }
+    )
+    }
+    $('form').on("submit", handleGetData)
